249377b59a6feada1e39a7349551a82a239c1dda
nikhil <sainikhil.voruganti@gmail.com>
2023-02-13 21:56:00 -0600

    basic version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+import About from './components/About';
+import TaskDetails from './components/TaskDetails';
+
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  // fetch returns a promise that we await
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, []) // pass an empty arr which could be a dependency arr
+
+// Fetch Tasks
+const fetchTasks = async () => {
+  const res = await fetch('http://localhost:8000/tasks')
+  const data = await res.json()
+
+  return data
+}
+
+// Fetch Task
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:8000/tasks/${id}`)
+  const data = await res.json()
+
+  return data
+}
+
+// Add Task
+const addTask = async (task) => {
+  // const id = Math.floor(Math.random() * 10000) + 1
+  // const newTask = {id, ...task}
+  // setTasks([...tasks, newTask])
+  const res = await fetch('http://localhost:8000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+}
+
+// Delete Task
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:8000/tasks/${id}`, {
+    method: 'DELETE',
+  })
+
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:8000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updatedTask)
+  })
+
+  const data = await res.json()
+
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder} : task))
+} 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        <Routes>
+          <Route path='/' element={
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')}
+            </>
+          } />
+          <Route path='/about' element={<About />} />
+          <Route path='/task/:id' element={<TaskDetails />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/TaskDetails.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams, Navigate, useNavigate, useLocation } from 'react-router-dom'
+import Button from './Button'
+
+function TaskDetails() {
+    const [loading, setLoading] = useState(true)
+    const [task, setTask] = useState({})
+    const [error, setError] = useState(null)
+
+    const params = useParams()
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
+
+    // TODO: Figure out continuous GET call
+    useEffect(() => {
+        const fetchTask = async () => {
+            const res = await fetch(`http://localhost:8000/tasks/${params.id}`)
+            const data = await res.json()
+
+            if(res.status == 404) {
+                // setError('Task not found')
+                navigate('/')
+            }
+
+            setTask(data)
+            setLoading(false)
+        }
+
+        fetchTask()
+    })
+
+    if (error) {
+        return <Navigate to='/' />
+    }
+
+    return loading ? (
+        <h3>Loading...</h3>
+    ) : (
+        <div>
+            <p>{pathname}</p>
+            <h3>{task.text}</h3>
+            <p>{task.day}</p>
+            <Button onClick={() => {
+                navigate(-1) //takes back to last page
+            }}
+            text='Go Back' />
+        </div>
+    )
+}
+
+export default TaskDetails
